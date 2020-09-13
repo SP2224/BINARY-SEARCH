2e145f9afe047c5cea5709aa1fd88ca5ae9a00e1
Satya Prakash Das <48172327+SP2224@users.noreply.github.com>
2020-09-14 01:54:50 +0530

    update main.py

--- Binary search in python/main.py
+++ Binary search in python/main.py
@@ -2,40 +2,33 @@
 """
 HAVE A LOOK ON THE ATTACHED IMAGE
 """
-#code, HERE L IS THE LOWEST ELEMENT AND R IS THE HIGHEST ELEMENT WHEREAS X IS AN GIVEN ELEMENT
+#code
 
-def binarySearch(arr, l, r, x):
-    # Check base case
-    if r >= l:
+def binarySearch(array, x, low, high):
 
-        mid = l + (r - l) // 2
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
 
-        # If element is present at the middle itself
-        if arr[mid] == x:
+        mid = low + (high - low)//2
+
+        if array[mid] == x:
             return mid
 
-        # If element is smaller than mid, then it
-        # can only be present in left subarray
-        elif arr[mid] > x:
-            return binarySearch(arr, l, mid - 1, x)
+        elif array[mid] < x:
+            low = mid + 1
 
-            # Else the element can only be present
-        # in right subarray
         else:
-            return binarySearch(arr, mid + 1, r, x)
+            high = mid - 1
 
-    else:
-        # Element is not present in the array
-        return -1
+    return -1
 
 
-arr = [2, 3, 4, 10, 40]
-x = 10
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
 
-# Function call
-result = binarySearch(arr, 0, len(arr) - 1, x)
+result = binarySearch(array, x, 0, len(array)-1)
 
 if result != -1:
-    print("Element is present at index % d" % result)
+    print("Element is present at index " + str(result))
 else:
-    print("Element is not present in array")
+    print("Not found")
